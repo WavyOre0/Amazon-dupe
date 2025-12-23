731de4b619aeabb4a28434e1209f5f75af73a2af
WavyOre0 <bobgotgames@gmail.com>
2025-12-23 08:46:36 -0600

    began testing money calculation

---
+++ tests/moneyTest.js
@@ -0,0 +1,21 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('test suite: formatCurrency');
+console.log('testing if converting works properly')
+if (formatCurrency(2095) === '20.95'){
+  console.log('working');
+} else {
+  console.log('failed');
+}
+console.log('works with 0')
+if (formatCurrency(0) === '0.00') {
+  console.log('worked');
+} else {
+  console.log('failed');
+}
+console.log('code rounds up to nearest cent')
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('worked');
+}
+
+
