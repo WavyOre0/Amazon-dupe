43b3061954cc051ef5e704d78ad3a9227699a7a5
WavyOre0 <bobgotgames@gmail.com>
2025-12-27 11:39:10 -0600

    practiced using promises instead of nesting in checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,35 @@ import { renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 
+Promise.all([
+  new Promise((resolve) => {//resolve is a function
+    loadProducts(() => {
+      resolve('value1');
+    });
+  })
+]).then ((value) => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  console.log(value);
+});
+
+
+/*
+new Promise((resolve) => {//resolve is a function
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  console.log(value);
+});
+
+ 
+
 loadProducts(() => {
   renderPaymentSummary();
   renderOrderSummary();
 });
+*/
