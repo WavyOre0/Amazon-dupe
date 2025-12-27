ada754b9244f08d79cbe1ee0705cf8f6ab6d0005
WavyOre0 <bobgotgames@gmail.com>
2025-12-27 13:21:03 -0600

    made place order button interactive and work correctly

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {cart} from '../../data/cart-class.js'
 import {getProduct} from '../../data/products.js'
 import { getDeliveryOption} from '../../data/deliveryOptions.js';
 import { formatCurrency } from '../utils/money.js';
+import { addOrder} from '../../data/orders.js';
 
 export function renderPaymentSummary () {
   let productPriceCents = 0;
@@ -59,11 +60,33 @@ export function renderPaymentSummary () {
       </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary
+    js-place-order">
       Place your order
     </button>
   `
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
 
+  document.querySelector('.js-place-order').addEventListener('click', async () => {
+    try{
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        //the actual data in the cart we want to send
+        body: JSON.stringify({
+          cart: cart.cartItems
+        })
+      });
+      const order = await response.json();
+      addOrder(order);
+      //console.log(order);
+    } catch (error) {
+      console.log('Unexpected error. Please try again later.')
+    }
+    
+    window.location.href = 'orders.html'
+  });
 };
 
